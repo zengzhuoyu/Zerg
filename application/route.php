04e910c1c348346b6341ed63e9c3e03901a453d2
zengzhuoyu <zhuoyuceng@creditease.cn>
2017-05-07 09:26:51 +0800

    第三章结束

---
+++ application/route.php
@@ -0,0 +1,34 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2016 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+//return [
+//    '__pattern__' => [
+//        'name' => '\w+',
+//    ],
+//    '[hello]'     => [
+//        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
+//        ':name' => ['index/hello', ['method' => 'post']],
+//    ],
+//
+//];
+
+use think\Route;
+
+//Route::rule('hello','sample/Test/hello','get',['https' => false]);
+
+//Route::rule('hello','sample/Test/hello','get|post',['https' => false]);
+
+//Route::get('hello','sample/Test/hello');
+//Route::post('hello','sample/Test/hello');
+//Route::any('hello','sample/Test/hello');
+
+//Route::get('hello/:id','sample/Test/hello');
+Route::post('hello/:id','sample/Test/hello');
