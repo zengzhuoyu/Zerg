a5e88a37ea5ffd5aa093183f5f74d191f70e7310
zengzhuoyu <zhuoyuceng@creditease.cn>
2017-05-07 16:22:27 +0800

    测试不用填写用户信息推送远程仓库

--- application/api/validate/IDMustBePositiveInt.php
+++ application/api/validate/IDMustBePositiveInt.php
@@ -18,8 +18,10 @@ class IDMustBePositiveInt extends BaseValidate{
     {
         //验证正整数规则
         if(is_numeric($value) && is_int($value + 0) && ($value + 0) > 0){
+            
             return true;
         }else{
+
             //自定义错误返回信息
             return $field.'必须是正整数';
         }
